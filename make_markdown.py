79c1b0a5cb6a4e6347479d2b6ad4b2a5cc975e22
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-09-24 17:34:50 -0700

    "summary.csv" is now the convention to list the encounter documentation for each patient.

--- make_markdown.py
+++ make_markdown.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 for num in range(1, 2):
     folder = f"./examples/patient_{num}"
-    df = pd.read_csv(f"{folder}/notes.csv")
+    df = pd.read_csv(f"{folder}/summary.csv")
     pretty = df.to_markdown(index=False)
 
-    with open(f"{folder}/notes.md", "w") as f:
+    with open(f"{folder}/summary.md", "w") as f:
         f.write(pretty)
