a854ff12326c7814c2397a7c7dfbbecf9125de87
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-09-24 21:52:04 -0700

    better README.md

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 These are synthetic notes -- **not** PHI (protected health information).
  
 ### Clinical audiences
+[README](examples/README.md) example patients.
+
 Intended for **Kidney Transplant** subject-matter-experts (SME) chart review practice/tutorials 
 * MD / providers
 * PhD / researchers
 * Medical school students
-* Other chart reviewers 
+* Other chart reviewers
+
+    
+
 
 ### Technical audiences
 Sites should load documents into LabelStudio for [Cumulus chart review](https://docs.smarthealthit.org/cumulus/chart-review/).
--- make_markdown.py
+++ make_markdown.py
@@ -1,9 +1,25 @@
 import pandas as pd
 
-for num in range(1, 10):
-    folder = f"./examples/patient_{num}"
-    df = pd.read_csv(f"{folder}/summary.csv")
-    pretty = df.to_markdown(index=False)
+def make_summary_md(num_patients=10):
+    for num in range(1, num_patients+1):
+        file_csv = f"./examples/patient_{num}/summary.csv"
+        file_md = f"./examples/patient_{num}/summary.md"
+        df = pd.read_csv(file_csv)
+        with open(file_md, "w") as f:
+            f.write(df.to_markdown(index=False))
 
-    with open(f"{folder}/summary.md", "w") as f:
-        f.write(pretty)
+def make_patient_list_md(num_patients=10):
+    file_csv = f"./examples/patient_list.csv"
+    file_md = f"./examples/README.md"
+
+    df = pd.read_csv(file_csv)
+    df["chatgpt"] = df["chatgpt"].str.strip().apply(lambda x: f"[chatgpt]({x})")
+    df["summary"] = df["patient_num"].str.strip().apply(lambda x: f"[summary]({x}/summary.md)")
+    df = df[["patient_num", "summary", "chatgpt", "description"]]
+
+    with open(file_md, "w") as f:
+        f.write(df.to_markdown(index=False))
+
+if __name__ == "__main__":
+    make_summary_md(9)
+    make_patient_list_md(9)
